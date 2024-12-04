3b6deca6837183c8e3517fa493c7cec6c26ad727
Sam Schwartz <obct537@gmail.com>
2024-12-04 11:13:22 -0500

    broken mongo commit

--- geohints2/backend/settings.py
+++ geohints2/backend/settings.py
@@ -86,8 +86,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'geohints',
+        'ENFORCE_SCHEMA': False,
+        'USERNAME': 'admin',
+        'PASSWORD': 'admin',
+        'CLIENT': {
+            'host': 'mongodb://localhost',  # Use the service name defined in docker-compose.yml
+            'port': 27017,
+        }
     }
 }
 
